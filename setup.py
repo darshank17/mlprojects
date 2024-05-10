03fa98e64df5c51bd234072d3683b7d199fdd920
darshank17 <darshankotecha17@gmail.com>
2024-05-11 01:31:56 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+Hipen_dot = '-e .'
+def get_requirements(file_path:str)->List[str]:
+  
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n" , "") for req in requirements]
+        
+        if Hipen_dot in requirements:
+            requirements.remove(Hipen_dot)
+    
+    return requirements
+
+setup(
+name = 'mlproject',
+version = '0.0.1',
+author = 'Darshan',
+author_email= 'darshankotecha17@gmail.com',
+packages=find_packages(),
+install_requires = get_requirements('requirements.txt')
+
+   
+)
